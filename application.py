ffb13f02ec120f1e600a4da4456ea21ee4682a89
ejennings25 <ejennin@yahoo.comgit config --global user.name ejennin>
2024-06-30 19:25:27 -0400

    add database

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 from flask_sqlalchemy import SQLAlchemy
 
@@ -6,8 +6,15 @@ from flask_sqlalchemy import SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 db = SQLAlchemy(app)
 
-# write crud for # id # book_name # author # publisher
+# added error handling using try-except block and jsonify
+@app.errorhandler(Exception)
+def handle_exception(error):
+    message = str(error)  # Capture the error message
+    return jsonify({'error': message}), 500  # Return JSON with error details
 
+
+
+# set the Book model with appropriate data types and constraint
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True, nullable=False)
@@ -16,6 +23,8 @@ class Book(db.Model):
 
     def __repr__(self):
         return f"{self.name} - {self.author} - {self.publisher}"
+    
+# write crud for # id # book_name # author # publisher
 
 @app.route('/')
 def index():
@@ -23,5 +32,12 @@ def index():
 
 @app.route('/books')
 def get_books():
+    books = Book.query.all()
 
-    return {"books": "book data"}
+    output = []
+    for book in books:
+        book_data = {'name': book.name, 'author': book.author, 'publisher': book.publisher}
+
+        output.append(book_data)
+
+    return {"books": output}
